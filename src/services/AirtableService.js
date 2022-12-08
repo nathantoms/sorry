d765b43e1539cda4ed6aa1c6391c85d5c6581226
Nathan <nathanetoms@gmail.com>
2022-12-08 00:46:51 +0000

    Add application/json to request

--- src/services/AirtableService.js
+++ src/services/AirtableService.js
@@ -2,8 +2,18 @@ import axios from "axios";
 
 export async function createRecord(aggrieved, perpetrator, eventDescription) {
   const config = {
-    headers: { Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}` }
+    headers: {
+      Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+      'Content-Type': 'application/json'
+    }
   };
+
+  // const config = {
+  //   headers: { 
+  //     Authorization: `Bearer pat2BtoVOcXBzjgmw.945f8f60a60e461a5b8663c1b54fbc1fe01111b56d0c309ef584f7f98ca1992b`,
+  //     'Content-Type': 'application/json'
+  //   }
+  // };
 
   const bodyParameters = {
     "records": [
