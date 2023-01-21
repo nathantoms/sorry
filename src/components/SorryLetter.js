93d0addde181962fdbe06950b78153a5a86cc949
Nathan Toms <nathanetoms@gmail.com>
2023-01-21 17:51:09 +0000

    Make it gender neutral

--- src/components/SorryLetter.js
+++ src/components/SorryLetter.js
@@ -35,7 +35,7 @@ export const SorryLetter = ({aggrieved, perpetrator, eventDescription, apologyId
       <>
         <p>Dear <strong>{aggrieved}</strong>,</p>
         <p><strong>{perpetrator}</strong>, and all of the Sonning Men's 2nd XI would like to formally apologise for <strong>"{eventDescription}"</strong>.</p>
-        <p><strong>{perpetrator}</strong> is working hard to correct this character flaw and the whole team will be supporting him wherever they can with this endeavour.</p>
+        <p><strong>{perpetrator}</strong> is working hard to correct this character flaw and the whole team will be supporting them wherever they can with this endeavour.</p>
         <p className="small-break">Warmest and sincerest apologies,</p>
         <p>Sonning Men's 2nd XI</p>
       </>
